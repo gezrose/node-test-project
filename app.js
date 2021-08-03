bc9bb01333a4681e884cb3f1aa41f7d9b504dcaa
Gerrard Rose <gez.rose@gmail.com>
2021-08-03 19:56:00 +0100

    web update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Welcome to the home page Justyna on EB!");
+    res.send("Welcome to the home page Justyna on EB version 2!");
 });
 
 const port = process.env.port || 3000;
